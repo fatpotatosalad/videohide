2c072e9d49cb1502cd12a02debf30a15caab08ca
fatpotatosalad <40069417+fatpotatosalad@users.noreply.github.com>
2021-10-04 15:32:49 +0545

    Fixed corrupt output

--- decoder.py
+++ decoder.py
@@ -11,9 +11,9 @@ out_path = args.out_dir
 base_in = os.path.basename(in_path)
 out_fil = os.path.join(out_path, os.path.splitext(base_in)[0])
 
-final_barr = b''
 video = cv2.VideoCapture(in_path)
 total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+break_next = False
 
 def progress(x):
     prog_steps = 50
@@ -27,10 +27,15 @@ with open(out_fil,'wb') as fil:
     cnt = 0
     while video.isOpened():
         success, frame = video.read()
-        try:
-            fil.write(bytearray(frame.flatten().tolist()))
-        except AttributeError:
-            break
+        if cnt + 1 == total_frames:
+            write_bytearray = bytearray(frame.flatten().tolist()).rstrip(b'\x00')
+            break_next = True
+        else:
+            write_bytearray = bytearray(frame.flatten().tolist())
+        fil.write(write_bytearray)
         progress(cnt)
+        if break_next:
+            break
         cnt += 1
+
 print('\nDone')
